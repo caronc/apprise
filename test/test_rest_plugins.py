5c38d3a8c79e326af1545b73e038b5d984732391
Chris Caron <lead2gold@gmail.com>
2019-09-09 19:21:37 -0400

    PopcornNotify Support

--- test/test_rest_plugins.py
+++ test/test_rest_plugins.py
@@ -4002,6 +4002,58 @@ TEST_URLS = (
     }),
 
     ##################################
+    # NotifyPopcorn (PopcornNotify)
+    ##################################
+    ('popcorn://', {
+        # No hostname/apikey specified
+        'instance': None,
+    }),
+    ('popcorn://{}/18001231234'.format('_' * 9), {
+        # invalid apikey
+        'instance': TypeError,
+    }),
+    ('popcorn://{}/1232348923489234923489234289-32423'.format('a' * 9), {
+        # invalid phone number
+        'instance': plugins.NotifyPopcornNotify,
+        'notify_response': False,
+    }),
+    ('popcorn://{}/abc'.format('b' * 9), {
+        # invalid email
+        'instance': plugins.NotifyPopcornNotify,
+        'notify_response': False,
+    }),
+    ('popcorn://{}/15551232000/user@example.com'.format('c' * 9), {
+        # value phone and email
+        'instance': plugins.NotifyPopcornNotify,
+    }),
+    ('popcorn://{}/15551232000/user@example.com?batch=yes'.format('w' * 9), {
+        # value phone and email with batch mode set
+        'instance': plugins.NotifyPopcornNotify,
+    }),
+    ('popcorn://{}/?to=15551232000'.format('w' * 9), {
+        # reference to to=
+        'instance': plugins.NotifyPopcornNotify,
+    }),
+    ('popcorn://{}/15551232000'.format('x' * 9), {
+        'instance': plugins.NotifyPopcornNotify,
+        # force a failure
+        'response': False,
+        'requests_response_code': requests.codes.internal_server_error,
+    }),
+    ('popcorn://{}/15551232000'.format('y' * 9), {
+        'instance': plugins.NotifyPopcornNotify,
+        # throw a bizzare code forcing us to fail to look it up
+        'response': False,
+        'requests_response_code': 999,
+    }),
+    ('popcorn://{}/15551232000'.format('z' * 9), {
+        'instance': plugins.NotifyPopcornNotify,
+        # Throws a series of connection and transfer exceptions when this flag
+        # is set and tests that we gracfully handle them
+        'test_requests_exceptions': True,
+    }),
+
+    ##################################
     # NotifyWebexTeams
     ##################################
     ('wxteams://', {
