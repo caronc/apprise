72d4cdedd008fe51f288b19cabaf1bfe49238113
Chris Caron <lead2gold@gmail.com>
2020-08-29 19:02:34 -0400

    A simple set of development and testing tools

---
+++ bin/checkdone.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+# Copyright (C) 2020 Chris Caron <lead2gold@gmail.com>
+# All rights reserved.
+#
+# This code is licensed under the MIT License.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files(the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions :
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# This is a useful tool for checking for any lint errors and additionally
+# checking the overall coverage.
+which flake8 &>/dev/null
+[ $? -ne 0 ] && \
+   echo "Missing flake8; make sure it is installed:" && \
+   echo "  >  pip install flake8" && \
+   exit 1
+
+which coverage &>/dev/null
+[ $? -ne 0 ] && \
+   echo "Missing coverage; make sure it is installed:" &&
+   echo "  >  pip install pytest-cov coverage" && \
+   exit 1
+
+flake8 . --show-source --statistics
+if [ $? -ne 0 ]; then
+   echo "PEP8 check failed"
+   exit 1
+fi
+
+# Run our unit test coverage check
+coverage run -m pytest -vv
+if [ $? -ne 0 ]; then
+   echo "Tests failed."
+   exit 1
+fi
+
+# Print our report
+coverage report --show-missing
