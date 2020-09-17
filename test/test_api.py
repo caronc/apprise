3952c2fafe80263ae7effa2dd9b386c2d995d11e
Chris Caron <lead2gold@gmail.com>
2020-09-17 16:55:48 -0400

    tests prove everything is working as intended

--- test/test_api.py
+++ test/test_api.py
@@ -1400,6 +1400,71 @@ def test_apprise_details_plugin_verification():
                 assert arg in defined_tokens
 
 
+@pytest.mark.skipif(sys.version_info.major <= 2, reason="Requires Python 3.x+")
+@mock.patch('requests.post')
+def test_apprise_async_mode(mock_post, tmpdir):
+    """
+    API: Apprise() async_mode tests
+
+    """
+    mock_post.return_value.status_code = requests.codes.ok
+
+    # Define some servers
+    servers = [
+        'xml://localhost',
+        'json://localhost',
+    ]
+
+    # Default Async Mode is to be enabled
+    asset = AppriseAsset()
+    assert asset.async_mode is True
+
+    # Load our asset
+    a = Apprise(asset=asset)
+
+    # add our servers
+    a.add(servers=servers)
+
+    # 2 servers loaded
+    assert len(a) == 2
+
+    # Our servers should carry this flag
+    for server in a:
+        assert server.asset.async_mode is True
+
+    # Send Notifications Asyncronously
+    assert a.notify("async") is True
+
+    # Provide an over-ride now
+    asset = AppriseAsset(async_mode=False)
+    assert asset.async_mode is False
+
+    # Load our asset
+    a = Apprise(asset=asset)
+
+    # Verify our configuration kept
+    assert a.asset.async_mode is False
+
+    # add our servers
+    a.add(servers=servers)
+
+    # 2 servers loaded
+    assert len(a) == 2
+
+    # Our servers should carry this flag
+    for server in a:
+        assert server.asset.async_mode is False
+
+    # Send Notifications Syncronously
+    assert a.notify("sync") is True
+
+    # Adjust 1 of the servers async_mode settings
+    a[0].asset.async_mode is True
+
+    # Send 1 Notification Syncronously, the other Asyncronously
+    assert a.notify("a mixed batch") is True
+
+
 def test_notify_matrix_dynamic_importing(tmpdir):
     """
     API: Apprise() Notify Matrix Importing
