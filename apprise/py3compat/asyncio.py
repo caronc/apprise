525f3869597be64ea2e6e5df95c6779dff31b431
Raman Gupta <7243222+raman325@users.noreply.github.com>
2020-09-29 21:27:19 -0400

    Run synchronous code asynchronously (#304)

--- apprise/py3compat/asyncio.py
+++ apprise/py3compat/asyncio.py
@@ -25,6 +25,8 @@
 
 import sys
 import asyncio
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
 from ..URLBase import URLBase
 from ..logger import logger
 
@@ -101,7 +103,12 @@ class AsyncNotifyBase(URLBase):
         Async Notification Wrapper
         """
         try:
-            return self.notify(*args, **kwargs)
+            loop = asyncio.get_event_loop()
+            with ThreadPoolExecutor() as executor:
+                return await loop.run_in_executor(
+                    executor,
+                    partial(self.notify, *args, **kwargs),
+                )
 
         except TypeError:
             # These our our internally thrown notifications
