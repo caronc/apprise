3c2300951731a16b08823f9db9d1c1fe42b17011
Miroslav Šedivý <sedivy.miro@gmail.com>
2021-07-29 23:20:34 +0200

    Revert "matrix: use webhook as mode according to docs."
    This reverts commit 3f5a7344349d2dc47f3fedcfc0f57dac77096471.

--- apprise/plugins/NotifyMatrix.py
+++ apprise/plugins/NotifyMatrix.py
@@ -1074,11 +1074,7 @@ class NotifyMatrix(NotifyBase):
             'image', NotifyMatrix.template_args['image']['default']))
 
         # Get our mode
-        if 'mode' in results['qsd']:
-            results['mode'] = results['qsd'].get('mode')
-
-        elif 'webhook' in results['qsd']:
-            results['mode'] = results['qsd'].get('webhook')
+        results['mode'] = results['qsd'].get('mode')
 
         # t2bot detection... look for just a hostname, and/or just a user/host
         # if we match this; we can go ahead and set the mode (but only if
