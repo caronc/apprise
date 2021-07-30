e13556dda6e008345cf63bb1376f3fd8994bd4c9
Stefan Reimer <stefan@zero-downtime.net>
2021-07-30 15:38:53 +0200

    Change NotifySlack to use new blocks feature to allow more markdown parsing and better layout

--- apprise/plugins/NotifySlack.py
+++ apprise/plugins/NotifySlack.py
@@ -364,23 +364,37 @@ class NotifySlack(NotifyBase):
         title = self._re_formatting_rules.sub(  # pragma: no branch
             lambda x: self._re_formatting_map[x.group()], title,
         )
-        body = self._re_formatting_rules.sub(  # pragma: no branch
-            lambda x: self._re_formatting_map[x.group()], body,
-        )
+        # Only for NONE markdown, otherwise eg links wont work
+        if self.notify_format != NotifyFormat.MARKDOWN:
+            body = self._re_formatting_rules.sub(  # pragma: no branch
+                lambda x: self._re_formatting_map[x.group()], body,
+            )
 
         # Prepare JSON Object (applicable to both WEBHOOK and BOT mode)
         payload = {
             'username': self.user if self.user else self.app_id,
-            # Use Markdown language
-            'mrkdwn': (self.notify_format == NotifyFormat.MARKDOWN),
             'attachments': [{
-                'title': title,
-                'text': body,
+                'blocks': [{
+                    'type': 'section',
+                    'text': {
+                        'type': 'mrkdwn' if self.notify_format == NotifyFormat.MARKDOWN else 'plain_text',
+                        'text': body
+                    }
+                }],
                 'color': self.color(notify_type),
-                # Time
-                'ts': time(),
-            }],
+            }]
         }
+
+        # Slack only accepts non-empty header sections
+        if title:
+            payload['attachments'][0]['blocks'].insert(0, {
+                'type': 'header',
+                'text': {
+                    'type': 'plain_text',
+                    'text': title,
+                    'emoji': True
+                }
+            })
 
         # Prepare our URL (depends on mode)
         if self.mode is SlackMode.WEBHOOK:
@@ -394,9 +408,28 @@ class NotifySlack(NotifyBase):
         else:  # SlackMode.BOT
             url = self.api_url.format('chat.postMessage')
 
+        # Include the footer only if specified to do so
         if self.include_footer:
-            # Include the footer only if specified to do so
-            payload['attachments'][0]['footer'] = self.app_id
+            _footer = {
+                'type': 'context',
+                'elements': [{
+                    'type': 'mrkdwn' if self.notify_format == NotifyFormat.MARKDOWN else 'plain_text',
+                    'text': self.app_id
+                }]
+            }
+
+            # Acquire our to-be footer icon if configured to do so
+            image_url = None if not self.include_image \
+                else self.image_url(notify_type)
+
+            if image_url:
+                _footer['elements'].insert(0, {
+                    'type': 'image',
+                    'image_url': image_url,
+                    'alt_text': notify_type
+                })
+
+            payload['attachments'][0]['blocks'].append(_footer)
 
         if attach and self.mode is SlackMode.WEBHOOK:
             # Be friendly; let the user know why they can't send their
@@ -452,16 +485,6 @@ class NotifySlack(NotifyBase):
                 # Store the valid and massaged payload that is recognizable by
                 # slack. This list is used for sending attachments later.
                 attach_channel_list.append(payload['channel'])
-
-            # Acquire our to-be footer icon if configured to do so
-            image_url = None if not self.include_image \
-                else self.image_url(notify_type)
-
-            if image_url:
-                payload['icon_url'] = image_url
-
-                if self.include_footer:
-                    payload['attachments'][0]['footer_icon'] = image_url
 
             response = self._send(url, payload)
             if not response:
