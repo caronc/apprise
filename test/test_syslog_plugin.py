b4de3081814899aa58fb9cd1097026ce7de2d645
Chris Caron <lead2gold@gmail.com>
2021-09-18 12:08:30 -0400

    syslog remote host support

--- test/test_syslog_plugin.py
+++ test/test_syslog_plugin.py
@@ -27,6 +27,7 @@ import re
 import pytest
 import mock
 import apprise
+import socket
 
 # Disable logging for a cleaner testing output
 import logging
@@ -49,6 +50,7 @@ def test_notify_syslog_by_url(openlog, syslog):
     assert obj.url().startswith('syslog://user') is True
     assert re.search(r'logpid=yes', obj.url()) is not None
     assert re.search(r'logperror=no', obj.url()) is not None
+    assert re.search(r'syslog://.*mode=local', obj.url())
 
     assert isinstance(
         apprise.Apprise.instantiate(
@@ -59,9 +61,11 @@ def test_notify_syslog_by_url(openlog, syslog):
     assert obj.url().startswith('syslog://user') is True
     assert re.search(r'logpid=no', obj.url()) is not None
     assert re.search(r'logperror=yes', obj.url()) is not None
+    assert re.search(r'syslog://.*mode=local', obj.url())
 
     # Test sending a notification
     assert obj.notify("body") is True
+    assert obj.notify(title="title", body="body") is True
 
     # Invalid Notification Type
     assert obj.notify("body", notify_type='invalid') is False
@@ -71,6 +75,7 @@ def test_notify_syslog_by_url(openlog, syslog):
     assert obj.url().startswith('syslog://local5') is True
     assert re.search(r'logpid=yes', obj.url()) is not None
     assert re.search(r'logperror=no', obj.url()) is not None
+    assert re.search(r'syslog://.*mode=local', obj.url())
 
     # Invalid instantiation
     assert apprise.Apprise.instantiate('syslog://_/?facility=invalid') is None
@@ -81,6 +86,7 @@ def test_notify_syslog_by_url(openlog, syslog):
     assert obj.url().startswith('syslog://daemon') is True
     assert re.search(r'logpid=yes', obj.url()) is not None
     assert re.search(r'logperror=no', obj.url()) is not None
+    assert re.search(r'syslog://.*mode=local', obj.url())
 
     # Facility can also be specified on the url as a hostname
     obj = apprise.Apprise.instantiate('syslog://kern?logpid=no&logperror=y')
@@ -88,11 +94,13 @@ def test_notify_syslog_by_url(openlog, syslog):
     assert obj.url().startswith('syslog://kern') is True
     assert re.search(r'logpid=no', obj.url()) is not None
     assert re.search(r'logperror=yes', obj.url()) is not None
+    assert re.search(r'syslog://.*mode=local', obj.url())
 
     # Facilities specified as an argument always over-ride host
     obj = apprise.Apprise.instantiate('syslog://kern?facility=d')
     assert isinstance(obj, apprise.plugins.NotifySyslog)
     assert obj.url().startswith('syslog://daemon') is True
+    assert re.search(r'syslog://.*mode=local', obj.url())
 
 
 @mock.patch('syslog.syslog')
@@ -109,6 +117,7 @@ def test_notify_syslog_by_class(openlog, syslog):
     assert obj.url().startswith('syslog://user') is True
     assert re.search(r'logpid=yes', obj.url()) is not None
     assert re.search(r'logperror=no', obj.url()) is not None
+    assert re.search(r'syslog://.*mode=local', obj.url())
 
     # Exception should be thrown about the fact no bot token was specified
     with pytest.raises(TypeError):
@@ -116,3 +125,78 @@ def test_notify_syslog_by_class(openlog, syslog):
 
     with pytest.raises(TypeError):
         apprise.plugins.NotifySyslog(facility=object)
+
+
+@mock.patch('syslog.syslog')
+@mock.patch('syslog.openlog')
+@mock.patch('socket.socket')
+def test_notify_syslog_remote(mock_socket, mock_openlog, mock_syslog):
+    """
+    API: Syslog Remote Testing
+
+    """
+    payload = "test"
+    mock_connection = mock.Mock()
+    # We add 4 bytes for the extra payload
+    mock_connection.sendto.return_value = len(payload) + 4
+    mock_socket.return_value = mock_connection
+
+    # localhost does not lookup to any of the facility codes so this
+    # gets interpreted as a host
+    obj = apprise.Apprise.instantiate('syslog://localhost')
+    assert isinstance(obj, apprise.plugins.NotifySyslog)
+    assert obj.url().startswith('syslog://localhost') is True
+    assert re.search(r'syslog://.*mode=remote', obj.url())
+
+    # Test with port
+    obj = apprise.Apprise.instantiate('syslog://localhost:518')
+    assert isinstance(obj, apprise.plugins.NotifySyslog)
+    assert obj.url().startswith('syslog://localhost:518') is True
+    assert re.search(r'syslog://.*mode=remote', obj.url())
+
+    # Test with default port
+    obj = apprise.Apprise.instantiate('syslog://localhost:514')
+    assert isinstance(obj, apprise.plugins.NotifySyslog)
+    assert obj.url().startswith('syslog://localhost') is True
+    assert re.search(r'syslog://.*mode=remote', obj.url())
+
+    # Specify a facility
+    obj = apprise.Apprise.instantiate('syslog://localhost/kern')
+    assert isinstance(obj, apprise.plugins.NotifySyslog)
+    assert obj.url().startswith('syslog://localhost/kern') is True
+    assert re.search(r'syslog://.*mode=remote', obj.url())
+
+    # Specify a facility requiring a lookup and having the port identified
+    # resolves any ambiguity
+    obj = apprise.Apprise.instantiate('syslog://kern:514/d')
+    assert isinstance(obj, apprise.plugins.NotifySyslog)
+    assert obj.url().startswith('syslog://kern/daemon') is True
+    assert re.search(r'syslog://.*mode=remote', obj.url())
+
+    # We can attempt to exclusively set the mode as well without a port
+    # to also remove ambiguity; this falls back to sending as the 'user'
+    obj = apprise.Apprise.instantiate('syslog://kern/d?mode=remote')
+    assert isinstance(obj, apprise.plugins.NotifySyslog)
+    assert obj.url().startswith('syslog://kern/daemon') is True
+    assert re.search(r'syslog://.*mode=remote', obj.url())
+
+    # Test notifications
+    # + 1 byte in size due to user
+    mock_connection.sendto.return_value = len(payload) + 5
+    assert obj.notify(body=payload) is True
+    # This only fails because the underlining sendto() will return a
+    # length different then what was expected
+    assert obj.notify(body="a different payload size") is False
+
+    # Test timeouts and errors that can occur
+    mock_connection.sendto.return_value = None
+    mock_connection.sendto.side_effect = socket.gaierror
+    assert obj.notify(body=payload) is False
+
+    mock_connection.sendto.side_effect = socket.timeout
+    assert obj.notify(body=payload) is False
+
+    with pytest.raises(TypeError):
+        # Handle an invalid mode
+        obj = apprise.Apprise.instantiate(
+            'syslog://user/?mode=invalid', suppress_exceptions=False)
