9f3be648ac34702763690eddb58f02877bacdf2d
Chris Caron <lead2gold@gmail.com>
2021-11-25 15:02:39 -0500

    removed module no longer being used

--- test/helpers/__init__.py
+++ test/helpers/__init__.py
@@ -22,9 +22,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-from .modules import ModuleImportError
 from .rest import AppriseURLTester
 
 __all__ = [
-    'ModuleImportError', 'AppriseURLTester',
+    'AppriseURLTester',
 ]
