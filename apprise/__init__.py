ec9d166572ba770df6a8aff2ec6f34e511587525
Chris Caron <lead2gold@gmail.com>
2022-04-28 19:20:56 -0400

    bumped version to v0.9.8.3

--- apprise/__init__.py
+++ apprise/__init__.py
@@ -24,7 +24,7 @@
 # THE SOFTWARE.
 
 __title__ = 'Apprise'
-__version__ = '0.9.8.2'
+__version__ = '0.9.8.3'
 __author__ = 'Chris Caron'
 __license__ = 'MIT'
 __copywrite__ = 'Copyright (C) 2022 Chris Caron <lead2gold@gmail.com>'
