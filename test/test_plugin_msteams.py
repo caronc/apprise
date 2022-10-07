cbd1dc7928781485e7cf98410cfe5ca048ae90da
Andreas Motl <andreas.motl@panodata.org>
2022-10-07 02:15:27 +0200

    Tests: Skip `test_msteams_yaml_config` on PyPy, it is flaky

--- test/test_plugin_msteams.py
+++ test/test_plugin_msteams.py
@@ -31,6 +31,7 @@ except ImportError:
     # Python 2.7
     import mock
 
+import sys
 import json
 import requests
 import pytest
@@ -389,6 +390,8 @@ def test_plugin_msteams_templating(mock_post, tmpdir):
         == 'http://localhost'
 
 
+@pytest.mark.skipif(
+    hasattr(sys, "pypy_version_info"), reason="Does not work reliably on PyPy")
 @mock.patch('requests.post')
 def test_msteams_yaml_config(mock_post, tmpdir):
     """
