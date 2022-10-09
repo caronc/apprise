85daf12fbd4bd25c139d453c56535e6c4fe46e2c
Andreas Motl <andreas.motl@panodata.org>
2022-10-09 13:49:36 +0200

    Chore: Remove Python 2 compatibility

--- test/test_plugin_bulksms.py
+++ test/test_plugin_bulksms.py
@@ -22,13 +22,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-try:
-    # Python 3.x
-    from unittest import mock
-
-except ImportError:
-    # Python 2.7
-    import mock
+from unittest import mock
 
 import requests
 from json import loads
