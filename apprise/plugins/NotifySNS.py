d8692ba300407e2fcee735ad0d5a10f82a12e969
Stephen Sennett <ssennett.au@gmail.com>
2022-12-31 14:03:10 +1100

    Extended SNS 403 Error Details

--- apprise/plugins/NotifySNS.py
+++ apprise/plugins/NotifySNS.py
@@ -60,9 +60,8 @@ IS_REGION = re.compile(
 
 # Extend HTTP Error Messages
 AWS_HTTP_ERROR_MAP = {
-    403: 'Unauthorized - Invalid Access/Secret Key Combination.',
+    403: 'Unauthorized - Invalid Access/Secret Key Combination, or insufficient IAM Permissions [`sns:Publish`,`sns:CreateTopic`]',
 }
-
 
 class NotifySNS(NotifyBase):
     """
