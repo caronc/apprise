fc49f8809587234605ae0aa46e1e7fa9f033d114
Joerg Schultze-Lutter <joerg.schultze.lutter@gmail.com>
2023-10-25 19:58:20 +0200

    - License Change from BSD-3 to BSD-2 - increased wait value for receive-after-send-to-socket routines

--- apprise/plugins/NotifyAprs.py
+++ apprise/plugins/NotifyAprs.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# BSD 3-Clause License
+# BSD 2-Clause License
 #
 # Apprise - Push Notification Library.
 # Copyright (c) 2023, Chris Caron <lead2gold@gmail.com>
@@ -14,10 +14,6 @@
 #    this list of conditions and the following disclaimer in the documentation
 #    and/or other materials provided with the distribution.
 #
-# 3. Neither the name of the copyright holder nor the names of its
-#    contributors may be used to endorse or promote products derived from
-#    this software without specific prior written permission.
-#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -29,9 +25,9 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-
+#
 # To use this plugin, you need to be a licensed ham radio operator
-
+#
 # Plugin constraints:
 #
 # - message length = 67 chars max.
@@ -144,7 +140,7 @@ class NotifyAprs(NotifyBase):
     # where the login response lacks information. In case you receive too many
     # "Rx: APRS-IS msg is too short - needs to have at least two lines" error
     # messages, you might want to increase this value to a larger time span
-    sleep_after_socket_send = 0.5
+    sleep_after_socket_send = 0.8
 
     # Once we have sent a packet to APRS-IS, these are the number seconds
     # that we will wait before we will continue with the next package
