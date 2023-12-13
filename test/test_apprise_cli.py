eb4e47cc45e40a32a3dddf31da9db5e996b95172
Alexandre Detiste <alexandre.detiste@gmail.com>
2023-12-13 23:39:21 +0100

    remove leftover bits of Python2 support (#1012)

--- test/test_apprise_cli.py
+++ test/test_apprise_cli.py
@@ -26,7 +26,6 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-from __future__ import print_function
 import re
 from unittest import mock
 
--- test/test_apprise_pickle.py
+++ test/test_apprise_pickle.py
@@ -26,7 +26,6 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-from __future__ import print_function
 import sys
 import pickle
 from apprise import Apprise, AppriseAsset, AppriseLocale
