ce5ffcd1e684cefe04a7c6815d37b5c2ae8cca42
Chris Caron <lead2gold@gmail.com>
2024-03-09 12:50:11 -0500

    add ref to api used to build support

--- apprise/plugins/NotifyLunaSea.py
+++ apprise/plugins/NotifyLunaSea.py
@@ -25,7 +25,10 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-
+#
+# API:
+#   https://docs.lunasea.app/lunasea/notifications/custom-notifications
+#
 import re
 import requests
 from json import dumps
